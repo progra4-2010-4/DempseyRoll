02d5b9e51be3c53bbb483a00012379d478186beb
Kuzuks <charlieeweed@hotmail.com>
2010-12-07 19:40:59 -0800

    mailer listo solo falta una duda

--- proyecto/app/mailers/reminder_mail.rb
+++ proyecto/app/mailers/reminder_mail.rb
@@ -1,3 +1,12 @@
 class ReminderMail < ActionMailer::Base
   default :from => "from@example.com"
+
+	def new_remind.me_email (note)
+		@note = note
+		@content = note.content
+		@email = note.email
+		@delivery = note.delivery
+
+		mail :to => @email, :subject => "Reminder!!" , #:body => @content
+	end
 end
