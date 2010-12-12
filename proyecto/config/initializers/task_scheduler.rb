baf2118427335d31a8c641ea796a3558028a5c13
Carlos Aguilar <caguilar@ubuntu.(none)>
2010-12-11 19:52:54 -0800

    scheduler con error con el envio de mail... en prueba

--- proyecto/config/initializers/task_scheduler.rb
+++ proyecto/config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
 
 scheduler.every '1m' do
-	puts 'yay'
-	#@note = Note.last
-	#ReminderMail.new_remind_me_email(@note).deliver
+	#puts 'yay'
+	@note = Note.last
+	ReminderMail.new_remind_me_email(@note).deliver
 end
