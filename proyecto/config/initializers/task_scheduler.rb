87dcbf6477b14efa1640ad095bda4a543df9163c
Carlos Aguilar <caguilar@ubuntu.(none)>
2010-12-11 20:30:53 -0800

    funcionando el mail con el scheduler, en prueba la revision de tiempo de envio

--- proyecto/config/initializers/task_scheduler.rb
+++ proyecto/config/initializers/task_scheduler.rb
@@ -4,7 +4,11 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
 
 scheduler.every '1m' do
-	#puts 'yay'
-	@note = Note.last
-	ReminderMail.new_remind_me_email(@note).deliver
+	@note = Note.all
+	Note.all.each do |n|
+		if (n.delivery - Time.at(0) <= 300 || n.delivery - Time.at(0) >= -300)
+			ReminderMail.new_remind_me_email(n).deliver
+			n.destroy
+		end
+	end
 end
