3a67d1154dd45635f51527f61f4bb73219a0563b
Carlos Aguilar <caguilar@ubuntu.(none)>
2010-12-11 17:48:16 -0800

    task del scheduler funcional, en proceso combinar la revision de mandar mail

--- proyecto/app/controllers/notes_controller.rb
+++ proyecto/app/controllers/notes_controller.rb
@@ -1,18 +1,11 @@
-require 'rubygems'
-require 'rufus/scheduler' 
-
 class NotesController < ApplicationController
 
   def create
-    scheduler = Rufus::Scheduler.start_new    
-
     @note = Note.new params[:note]
 
     if @note.save
 	redirect_to new_note_path, :notice => "Success!"
-	scheduler.at @note.delivery do
-    		ReminderMail.new_remind_me_email(@note).deliver
-  	end
+    	#ReminderMail.new_remind_me_email(@note).deliver
     else
 	redirect_to new_note_path, :notice => "You left one or more fields empty, try again."
     end
---
+++ proyecto/config/initializers/task_scheduler.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'rufus/scheduler' 
+
+scheduler = Rufus::Scheduler.start_new
+
+scheduler.every '1m' do
+	puts 'yay'
+	#@note = Note.last
+	#ReminderMail.new_remind_me_email(@note).deliver
+end
